e3567a4e9f59f697133bdc8d28cfca24bf3146f5
pukv <pukvavlovic10@gmail.com>
2024-09-01 15:11:07 +0200

    added comments with some explanation and changed the i -> to rounds for clarification

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 function getComputerChoice() {
-  let computerPick = Math.random();
+  let computerPick = Math.random(); // as there are 3 possible choices, the number 1 will be divided into 3 equal parts
   if (computerPick <= 0.33) {
     return "Rock";
   } else if (computerPick <= 0.66) {
@@ -11,7 +11,7 @@ function getComputerChoice() {
 
 function getHumanChoice() {
   let humanChoice = prompt("Rock Paper Scissors?");
-  // make the users input be case insensitive
+  // Make the user input not case sensitive
   humanChoice =
     humanChoice[0].toUpperCase() + humanChoice.slice(1).toLowerCase();
   if (
@@ -21,11 +21,12 @@ function getHumanChoice() {
   ) {
     return humanChoice;
   } else {
-    alert("Please pick a valid option.");
+    alert("Please pick a valid option."); // Prompt if the user enters something like 'stone' or any other word that's not legit
     return getHumanChoice();
   }
 }
 
+// Variables that store the score
 let humanScore = 0;
 let computerScore = 0;
 
@@ -33,6 +34,7 @@ function playRound(humanChoice, computerChoice) {
   if (humanChoice === computerChoice) {
     console.log(`It's a tie. You both picked ${humanChoice}`);
   } else if (
+    // Conditions in which the user wins the game
     (humanChoice === "Rock" && computerChoice === "Scissors") ||
     (humanChoice === "Paper" && computerChoice === "Rock") ||
     (humanChoice === "Scissors" && computerChoice === "Paper")
@@ -40,17 +42,17 @@ function playRound(humanChoice, computerChoice) {
     console.log(
       `You picked ${humanChoice}, the Computer picked ${computerChoice}. You win.`
     );
-    humanScore++;
+    humanScore++; // Increments the user score by +1
   } else {
     console.log(
       `You picked ${humanChoice}, the Computer picked ${computerChoice}. The computer wins.`
     );
-    computerScore++;
+    computerScore++; // Increments the computer score by +1
   }
 }
 
 function playGame() {
-  for (let i = 0; i < 5; i++) {
+  for (let round = 0; round < 5; round++) {
     let humanSelection = getHumanChoice();
     let computerSelection = getComputerChoice();
     playRound(humanSelection, computerSelection);
