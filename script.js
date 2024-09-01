a8db506dc5934826c2d57e31269ed29a4074edb4
pukv <pukvavlovic10@gmail.com>
2024-09-01 03:56:21 +0200

    added logic for computer choice

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
-
+function getComputerChoice() {
+  let computerPick = Math.random();
+  if (computerPick <= 0.33) {
+    console.log("Computer picked Rock!");
+  } else if (computerPick >= 0.33 && computerPick >= 0.66) {
+    console.log("Computer picked Paper!");
+  } else {
+    console.log("Computer picked scissors!");
+  }
+}
